202c41633917e8a5e01366ed4886e1b6e2f8db92
Percy Hatcherson <percy.hatcherson@gmail.com>
2015-05-17 11:37:14 -0500

    Conver bill model to ES6 class.

--- src/models/bill.js
+++ src/models/bill.js
@@ -1,42 +1,44 @@
+'use strict';
+
 var Model = require('../models/model'),
   config = require('../../config');
 
-// Constructor
-function Bill(){
-  this.query = {
-    apikey: config.apiKey
-  };
-  this.endpoint = '/bills';
-  this.url = config.urls.congress;
+class Bill extends Model {
+    constructor() {
+        this.query = {
+          apikey: config.apiKey
+        };
+        this.endpoint = '/bills';
+        this.url = config.urls.congress;
+    }
+
+    findById (query, callback) {
+      this.url += query.id;
+      this.find(query, callback);
+    }
+
+    findByChamber(query, callback) {
+
+      if (query.chamber === 'senate') {
+          query['history.senate_passage_result__exists'] = true;
+      } else {
+          query['history.house_passage_result__exists'] = true;
+      }
+      this.find( query, callback );
+    }
+
+    formatResponse(body) {
+      var responseData = {};
+      responseData = JSON.parse( body ).results;
+
+      responseData = responseData.filter(function(item) {
+        // `undefined != null` coerces undefined to null
+        return item.bill_id != null;
+      });
+
+      return {bills: responseData};
+    }
+
 }
-
-Bill.prototype = new Model();
-
-Bill.prototype.findById = function(query, callback) {
-  this.url += query.id;
-  this.find(query, callback);
-};
-
-Bill.prototype.findByChamber = function(query, callback){
-
-  if (query.chamber === 'senate') {
-      query['history.senate_passage_result__exists'] = true;
-  } else {
-      query['history.house_passage_result__exists'] = true;
-  }
-  this.find( query, callback );
-};
-
-Bill.prototype.formatResponse = function(body) {
-  var responseData = {};
-  responseData = JSON.parse( body ).results;
-
-  responseData = responseData.filter(function(item) {
-    // `undefined != null` coerces undefined to null
-    return item.bill_id != null;
-  });
-
-  return {bills: responseData};
-};
 
 module.exports = Bill;
